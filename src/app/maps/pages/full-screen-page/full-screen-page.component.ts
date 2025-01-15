5fed67733379495c3d486d3b4327f10aa1900740
annruiblaz <anruiblaz@gmail.com>
2025-01-15 22:35:55 +0100

    import  + fullscreen mapa añadido

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+import maplibregl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
 
 @Component({
   templateUrl: './full-screen-page.component.html',
   styleUrl: './full-screen-page.component.css'
 })
-export class FullScreenPageComponent {
+export class FullScreenPageComponent implements AfterViewInit{
+  @ViewChild('map',{static: false})
+  public divMap?: ElementRef;
+
+  ngAfterViewInit(): void {
+
+    console.log(this.divMap);
+
+    if(!this.divMap) {
+      throw 'El elemento html no se pudo encontrar.';
+    }
+
+    const map = new maplibregl.Map({
+      container: this.divMap.nativeElement,
+      style: 'https://demotiles.maplibre.org/style.json', // style URL
+      center: [0, 0], // starting position [lng, lat]
+      zoom: 1 // starting zoom
+    });
+
+/*     console.log(this.divMap);
+ */
+  }
 
 }
