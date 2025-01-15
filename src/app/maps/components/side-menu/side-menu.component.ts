9556f4061e7c15c9c6f11f1a790f53df08e919b9
annruiblaz <anruiblaz@gmail.com>
2025-01-15 21:01:43 +0100

    inicio proyecto + rutas

---
+++ src/app/maps/components/side-menu/side-menu.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { MenuItem } from '../../interfaces/interfaces';
+
+@Component({
+  selector: 'maps-side-menu',
+  templateUrl: './side-menu.component.html',
+  styleUrl: './side-menu.component.css'
+})
+export class SideMenuComponent {
+  public menuItems: MenuItem[] = [
+    {route: '/maps/fullscreen', name: 'FullScreen'},
+    {route: '/maps/zoom-range', name: 'Zoom-Range'},
+    {route: '/maps/markers', name: 'Markers'},
+    {route: '/maps/properties', name: 'Properties'},
+
+  ]
+}
